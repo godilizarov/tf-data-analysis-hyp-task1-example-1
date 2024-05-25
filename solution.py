e1ee777e432ef39c5500bd6a1343ac0192464503
godilizarov <90186460+godilizarov@users.noreply.github.com>
2024-05-25 22:51:11 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,21 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 419801345 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+               
+    p = (x_success + y_success) / (x_cnt + y_cnt)
+  
+    se = (p * (1 - p) * (1/x_cnt + 1/y_cnt)) ** 0.5
+    
+    z = (p1 - p2) / se
+    
+    z_critical = stats.norm.ppf(1 - 0.05)  
+               
+    return abs(z) > z_critical
